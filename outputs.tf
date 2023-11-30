58e5eebf2a0ebd65f2f5a1545da9b4bfef16222b
ptonini <pedro.tonini@hotmail.com>
2023-11-30 18:45:25 -0300

    fix: added kms_key_id to output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "ssh_master_key" {
   value = one(aws_key_pair.this)
 }
 
+output "kms_key_id" {
+  value = aws_kms_key.this.id
+}
+
 output "s3_inventory_bucket_arn" {
   value = module.s3_inventory_bucket.this.arn
 }
